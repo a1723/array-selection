545315dbe3823541e8d4d4d78e61a36f3da40455
Aleksandr K <goden12345@mail.ru>
2020-01-10 16:58:12 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "container-fluid" style="width: 140rem;">
+        <form action="script.php" method = "POST">
+        <p class = "lead"> <mark> После нажатия на эту кнопку будет сгенерирован рандомный массив из 100 элементов <br> и выведено на экран количество последовательных пар одинаковых элементов массива </mark> </p>
+        <input class="btn btn-primary btn-sm" type = "submit" value = "Отправить запрос">
+        </form>
+    </div>
+</body>
+</html>
